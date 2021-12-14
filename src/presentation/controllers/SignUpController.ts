a2c979b5e74fa5c36dbec7bb26d79a627d2fe1e9
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-14 12:45:01 -0300

    feat: use email interface in scope

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,10 +1,16 @@
-import { MissingParamError } from '../errors'
+import { InvalidParamError, MissingParamError } from '../errors'
 import badRequest from '../helpers/badRequest'
 import Controller from '../protocols/Controller'
+import EmailValidator from '../protocols/EmailValidator'
 import { HttpRequest, HttpResponse } from '../protocols/HttpAnnouncements'
 
 class SignUpController implements Controller {
+  constructor (
+    private readonly emailValidator: EmailValidator
+  ) { }
+
   handle (httpRequest: HttpRequest): HttpResponse {
+    const { email } = httpRequest.body
     const fields = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const field of fields) {
@@ -13,6 +19,12 @@ class SignUpController implements Controller {
           new MissingParamError(`Missing param: ${field}`)
         )
     }
+
+    const emailIsValid = this
+      .emailValidator
+      .isValid(email as string)
+    if (!emailIsValid)
+      return badRequest(new InvalidParamError('email'))
   }
 }
 
