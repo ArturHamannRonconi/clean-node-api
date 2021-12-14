01b6b08445c75a01b25e9702cec80c59835918a4
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-14 12:19:01 -0300

    feat: add Invalid Param Error

---
+++ src/presentation/errors/InvalidParamError.ts
@@ -0,0 +1,9 @@
+class InvalidParamError extends Error {
+  constructor (fieldName: string) {
+    super(`Invalid param: ${fieldName}`)
+    Object.setPrototypeOf(this, InvalidParamError.prototype)
+    this.name = 'InvalidParamError'
+  }
+}
+
+export default InvalidParamError
