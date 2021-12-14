3fce5861e8ac0addd46fef13641bb1719a7f989e
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-14 11:57:45 -0300

    test: ensure return 400 password and passwordConfirmation

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -31,4 +31,34 @@ describe('SignUp Controller', () => {
     expect(httpResponse).toHaveProperty('statusCode', 400)
     expect(httpResponse).toHaveProperty('body', new MissingParamError('Missing param: email'))
   })
+
+  it('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse).toHaveProperty('statusCode', 400)
+    expect(httpResponse).toHaveProperty('body', new MissingParamError('Missing param: password'))
+  })
+
+  it('Should return 400 if no passwordConfirmation is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse).toHaveProperty('statusCode', 400)
+    expect(httpResponse).toHaveProperty('body', new MissingParamError('Missing param: passwordConfirmation'))
+  })
 })
