b9c353f38c5bb6ae3e9cc04b1d39a1ed2afd55b8
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-14 11:07:48 -0300

    test: initial SignUpController

---
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -0,0 +1,18 @@
+import SignUpController from './SignUpController'
+
+describe('SignUp Controller', () => {
+  it('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse).toHaveProperty('statusCode', 400)
+    expect(httpResponse).toHaveProperty('body', new Error('Missing param: name'))
+  })
+})
