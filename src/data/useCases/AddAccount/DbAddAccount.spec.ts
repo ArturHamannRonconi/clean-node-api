b3b74810609543372b85f666df258335e0065328
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-15 09:59:49 -0300

    test: ensure password are hashed before inserts database

---
+++ src/data/useCases/AddAccount/DbAddAccount.spec.ts
@@ -0,0 +1,42 @@
+import { Encrypter } from '../../protocols/Encrypter'
+import { DbAddAccount } from './DbAddAccount'
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+
+  return new EncrypterStub()
+}
+
+interface SutTypes {
+  sut: DbAddAccount
+  encryptStub: Encrypter
+}
+
+const makeSUT = (): SutTypes => {
+  const encryptStub = makeEncrypter()
+  const sut = new DbAddAccount(encryptStub)
+  return {
+    sut,
+    encryptStub
+  }
+}
+
+describe('DbAddAccount UseCase', () => {
+  it('Should call Hash with correct password', async () => {
+    const { sut, encryptStub } = makeSUT()
+    const encryptSpy = jest.spyOn(encryptStub, 'encrypt')
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    await sut.add(accountData)
+    expect(encryptSpy).toHaveBeenCalledWith(accountData.password)
+  })
+})
