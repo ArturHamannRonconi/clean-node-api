b1bbb5561e6676b2421999d68a92823942cb042b
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-15 16:45:41 -0300

    test: ensure cors is enable

---
+++ src/main/middlewares/cors/Cors.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+const route = '/test_cors'
+
+describe('Cors Middleware', () => {
+  it('Should enable cors', async () => {
+    app.post(route, (req, res) => res.json({ a: 'a' }))
+
+    await request(app)
+      .post(route)
+      .expect('access-control-allow-origin', '*')
+  })
+})
