f372506727412654721e61eb887e59881c3f3930
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-16 15:23:51 -0300

    test: ensure get required fields by body

---
+++ src/presentation/controllers/SignInController/SignInController.spec.ts
@@ -0,0 +1,37 @@
+import { SignInController } from '.'
+import { MissingParamError } from '../../errors'
+import { StatusCode } from '../../protocols'
+
+const makeSUT = (): SignInController => {
+  return new SignInController()
+}
+
+describe('Sign In Controller', () => {
+  it('Should be able to return 400 if email doesn\'t sended', async () => {
+    const sut = makeSUT()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toHaveProperty('statusCode', StatusCode.BAD_REQUEST)
+    expect(httpResponse).toHaveProperty('body', new MissingParamError('email'))
+  })
+
+  it('Should be able to return 400 if password doesn\'t sended', async () => {
+    const sut = makeSUT()
+    const httpRequest = {
+      body: {
+        email: 'any_email'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toHaveProperty('statusCode', StatusCode.BAD_REQUEST)
+    expect(httpResponse).toHaveProperty('body', new MissingParamError('password'))
+  })
+})
