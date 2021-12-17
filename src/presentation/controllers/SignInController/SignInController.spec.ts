59d823d3bfc71cc4a84d7f6b6128ed9abf4ecd02
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-17 15:31:22 -0300

    test: ensure Sign In controller return tokens

--- src/presentation/controllers/SignInController/SignInController.spec.ts
+++ src/presentation/controllers/SignInController/SignInController.spec.ts
@@ -1,25 +1,42 @@
 import { SignInController } from '.'
 import { StatusCode } from '../../protocols'
-import { MissingParamError } from '../../errors'
+import { MissingParamError, ServerError } from '../../errors'
 import { RequiredFieldsValidator } from '../../protocols/RequiredFieldsValidator'
 import { RequiredFieldsValidatorAdapter } from '../../utils/RequiredFieldsValidatorAdapter/RequiredFieldsValidatorAdapter'
+import { AuthenticationRequestDTO, AuthenticationResponseDTO, AuthenticationUseCase } from '../../../domain/useCases/AuthenticationUseCase'
+import { UnautorizedError } from '../../errors/UnautorizedError'
+
+const makeAuthentication = (): AuthenticationUseCase => {
+  class AuthenticationStub implements AuthenticationUseCase {
+    async auth (authentication: AuthenticationRequestDTO): Promise<AuthenticationResponseDTO> {
+      return await new Promise(resolve => resolve({ accessToken: 'any_token' }))
+    }
+  }
+
+  return new AuthenticationStub()
+}
 
 interface SutTypes {
   sut: SignInController
   requiredFieldsValidator: RequiredFieldsValidator
+  authenticationUseCaseStub: AuthenticationUseCase
 }
 
 const makeSUT = (): SutTypes => {
   const requiredFieldsValidator = new RequiredFieldsValidatorAdapter([
     'email', 'password'
   ])
+  const authenticationUseCaseStub = makeAuthentication()
+
   const sut = new SignInController(
-    requiredFieldsValidator
+    requiredFieldsValidator,
+    authenticationUseCaseStub
   )
 
   return {
     sut,
-    requiredFieldsValidator
+    requiredFieldsValidator,
+    authenticationUseCaseStub
   }
 }
 
@@ -50,5 +67,71 @@ describe('Sign In Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toHaveProperty('statusCode', StatusCode.BAD_REQUEST)
     expect(httpResponse).toHaveProperty('body', new MissingParamError('password'))
+  })
+
+  it('Should call Authentication with correct values', async () => {
+    const { sut, authenticationUseCaseStub } = makeSUT()
+    const authSpy = jest.spyOn(authenticationUseCaseStub, 'auth')
+
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(authSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  it('Should return token if authentication return token', async () => {
+    const { sut, authenticationUseCaseStub } = makeSUT()
+    jest.spyOn(authenticationUseCaseStub, 'auth')
+
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.body).toHaveProperty('accessToken', 'any_token')
+  })
+
+  it('Should return 500 if authentication is failed', async () => {
+    const { sut, authenticationUseCaseStub } = makeSUT()
+    jest
+      .spyOn(authenticationUseCaseStub, 'auth')
+      .mockImplementation(() => { throw new Error() })
+
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toHaveProperty('statusCode', StatusCode.INTERNAL_SERVER)
+    expect(httpResponse).toHaveProperty('body', new ServerError())
+  })
+
+  it('Should return 401 if invalid credentials provided', async () => {
+    const { sut, authenticationUseCaseStub } = makeSUT()
+    jest
+      .spyOn(authenticationUseCaseStub, 'auth')
+      .mockReturnValueOnce(new Promise(resolve => resolve(null)))
+
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse)
+      .toHaveProperty('statusCode', StatusCode.UNAUTORIZED)
+    expect(httpResponse)
+      .toHaveProperty('body', new UnautorizedError())
   })
 })
