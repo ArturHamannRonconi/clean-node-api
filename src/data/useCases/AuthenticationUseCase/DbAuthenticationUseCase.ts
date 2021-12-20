a9ceb75889ba7deda4b07d57556c97d587a92d51
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-20 16:24:42 -0300

    refactor: ensure DbAuthentication return somewhere in the end

--- src/data/useCases/AuthenticationUseCase/DbAuthenticationUseCase.ts
+++ src/data/useCases/AuthenticationUseCase/DbAuthenticationUseCase.ts
@@ -8,12 +8,14 @@ class DbAuthenticationUseCase implements AuthenticationUseCase {
   ) { }
 
   async auth ({ email, password }: AuthenticationRequestDTO): Promise<AuthenticationResponseDTO> {
-    const account = await this.findAccountRepository.byEmail(email)
-
+    const account = await this
+      .findAccountRepository.byEmail(email)
     if (!account) return null
 
-    await this.encrypter.compare(password, account.password)
-    return null
+    const correctPassword = await this.encrypter
+      .compare(password, account.password)
+
+    if (!correctPassword) return null
   }
 }
 
