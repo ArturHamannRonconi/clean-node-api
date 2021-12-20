5d03314780d8e4ff2362f1d622809defb75f916c
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-20 18:40:10 -0300

    refactor: move jwt dependencies to constructor

--- src/infra/Authentication/AuthenticateJWTAdapter.spec.ts
+++ src/infra/Authentication/AuthenticateJWTAdapter.spec.ts
@@ -1,7 +1,26 @@
 import jwt from 'jsonwebtoken'
+import { Authenticate } from '../../data/protocols'
 import { AuthenticateJWTAdapter } from './AuthenticateJWTAdapter'
 
-const { JWT_SECRET } = process.env
+interface SutTypes {
+  sut: Authenticate
+  secret: string
+  expiresIn: string | number
+}
+
+const makeSUT = (): SutTypes => {
+  const secret = 'JWT_SECRET'
+  const expiresIn = '15m'
+  const sut = new AuthenticateJWTAdapter(
+    secret, expiresIn
+  )
+
+  return {
+    sut,
+    secret,
+    expiresIn
+  }
+}
 
 jest.mock('jsonwebtoken', () => ({
   verify: () => true,
@@ -10,15 +29,14 @@ jest.mock('jsonwebtoken', () => ({
 
 describe('Authenticate JWT Adapter', () => {
   it('Should be able to call jwt sign with correct values', async () => {
-    const sut = new AuthenticateJWTAdapter()
+    const { sut, secret, expiresIn } = makeSUT()
     const signSpy = jest.spyOn(jwt, 'sign')
     const id = 'any_id'
 
     await sut.auth(id)
     expect(signSpy).toHaveBeenCalledWith(
-      { userId: id },
-      JWT_SECRET,
-      { subject: id, expiresIn: '15m' }
+      { userId: id }, secret,
+      { subject: id, expiresIn }
     )
   })
 })
--- src/infra/Authentication/AuthenticateJWTAdapter.ts
+++ src/infra/Authentication/AuthenticateJWTAdapter.ts
@@ -5,12 +5,17 @@ import { Tokens } from '../../data/protocols/Authenticate/Tokens'
 import { Guid } from '../../domain/protocols/Guid'
 
 class AuthenticateJWTAdapter implements Authenticate {
+  constructor (
+    private readonly JWT_SECRET: string,
+    private readonly expresIn: string | number
+  ) { }
+
   async auth (id: Guid): Promise<Tokens> {
     const userId = id as string
 
     const accessToken = sign(
-      { userId }, process.env.JWT_SECRET,
-      { subject: userId, expiresIn: '15m' }
+      { userId }, this.JWT_SECRET,
+      { subject: userId, expiresIn: this.expresIn }
     )
 
     return { accessToken }
