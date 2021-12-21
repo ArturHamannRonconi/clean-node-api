4e3633c4c526c77c7e9271ca75880146e8678cb0
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-21 11:22:57 -0300

    test: ensure login reoute are created

--- src/main/routes/loginRoutes/login.routes.test.ts
+++ src/main/routes/loginRoutes/login.routes.test.ts
@@ -2,10 +2,18 @@ import request from 'supertest'
 
 import { app } from '../../app'
 import { MongoHelperConnection } from '../../../infra/database/mongodb/helpers/MongoHelperConnection'
+import { SignUpHttpRequestBody } from '../../../presentation/controllers/SignUpController/SignUpHttpRequestBody'
 
 const { MONGO_URL } = process.env
 
-describe('Sign Up routes', () => {
+const fakeAccount = (): SignUpHttpRequestBody => ({
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'a44638qewq92!A',
+  passwordConfirmation: 'a44638qewq92!A'
+})
+
+describe('Login routes', () => {
   beforeAll(async () => await MongoHelperConnection.connect(MONGO_URL))
   afterAll(async () => await MongoHelperConnection.disconnect())
   afterEach(async () => {
@@ -15,17 +23,29 @@ describe('Sign Up routes', () => {
     await collection.deleteMany({})
   })
 
-  describe('POST Sign Up', () => {
+  describe('Sign Up', () => {
     it('Should return 201 on success', async () => {
       await request(app)
         .post('/api/v1/login/sign-up')
-        .send({
-          name: 'any_name',
-          email: 'any_email@mail.com',
-          password: 'a44638qewq92!A',
-          passwordConfirmation: 'a44638qewq92!A'
-        })
+        .send(fakeAccount())
         .expect(201)
+    })
+  })
+
+  describe('Sign In', () => {
+    it('Should return 200 on success', async () => {
+      const { email, password } = fakeAccount()
+
+      await request(app)
+        .post('/api/v1/login/sign-up')
+        .send(fakeAccount())
+
+      const response = await request(app)
+        .post('/api/v1/login/sign-in')
+        .send({ email, password })
+        .expect(200)
+
+      expect(response.body).toHaveProperty('accessToken')
     })
   })
 })
