7352d43b0f697e10c5461504a3893785f1b251f9
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-21 10:53:27 -0300

    refactor: use express adapter into SignUpControllerExpressAdapter

--- src/infra/router/express/SignUpControllerExpressAdapter.ts
+++ src/infra/router/express/SignUpControllerExpressAdapter.ts
@@ -1,22 +1,15 @@
 import { Request, Response } from 'express'
+import { ExpressAdapter } from './ExpressAdapter'
 import { signUpFactory } from '../../../main/factories/SignUpFactory'
+import { SignUpHttpRequestBody } from '../../../presentation/controllers/SignUpController/SignUpHttpRequestBody'
 
 class SignUpControllerExpressdapter {
   static async handle (request: Request, response: Response): Promise<Response> {
-    const httpRequest = {
-      body: request.body,
-      headers: request.headers
-    }
-    const resultResponse = await signUpFactory().handle(httpRequest)
-
-    if (resultResponse.headers && resultResponse?.headers.length > 0)
-      resultResponse.headers.forEach(
-        ({ headerName, value }) => response.setHeader(headerName, value)
-      )
-
-    return response
-      .status(resultResponse?.statusCode || 200)
-      .json(resultResponse?.body || {})
+    return await ExpressAdapter.transformToExpressRequest<SignUpHttpRequestBody>(
+      request,
+      response,
+      signUpFactory
+    )
   }
 }
 
