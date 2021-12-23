95fa85cdd751fef8e111ce49773e45f7a834dc3e
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-23 13:45:48 -0300

    test: ensure AuthorizationMiddleware calls ConfirmAccessTokenUseCase

--- src/presentation/middlewares/AuthorizationMiddleware/AuthorizationMiddleware.spec.ts
+++ src/presentation/middlewares/AuthorizationMiddleware/AuthorizationMiddleware.spec.ts
@@ -1,16 +1,47 @@
-import { AccessDeniedError } from '../../utils/errors'
 import { StatusCode } from '../../protocols/http'
+import { AccessDeniedError } from '../../utils/errors'
 import { AuthorizationMiddleware } from './AuthorizationMiddleware'
+import { HttpRequest } from '../../protocols/http/HttpAnnouncements'
+import { ConfirmAccessTokenRequestDTO, ConfirmAccessTokenResponseDTO, ConfirmAccessTokenUseCase } from '../../../domain/useCases/ConfirmAccessTokenUseCase'
+
+const makeFakeConfirmAccessTokenUseCase = (): ConfirmAccessTokenUseCase => {
+  class ConfirmAccessTokenUseCaseStub implements ConfirmAccessTokenUseCase {
+    async confirm (accessToken: ConfirmAccessTokenRequestDTO): Promise<ConfirmAccessTokenResponseDTO> {
+      return await new Promise(
+        resolve => resolve({
+          account: {
+            id: 'any_id',
+            name: 'any_name',
+            email: 'any_email',
+            password: 'any_password'
+          }
+        })
+      )
+    }
+  }
+
+  return new ConfirmAccessTokenUseCaseStub()
+}
+
+const makeFakeHttpRequest = (): HttpRequest<any> => ({
+  headers: { authorization: 'any_token' },
+  body: {}
+})
 
 interface SutTypes {
   sut: AuthorizationMiddleware<any>
+  confirmAccessTokenUseCase: ConfirmAccessTokenUseCase
 }
 
 const makeSUT = (): SutTypes => {
-  const sut = new AuthorizationMiddleware()
+  const confirmAccessTokenUseCase = makeFakeConfirmAccessTokenUseCase()
+  const sut = new AuthorizationMiddleware(
+    confirmAccessTokenUseCase
+  )
 
   return {
-    sut
+    sut,
+    confirmAccessTokenUseCase
   }
 }
 
@@ -21,5 +52,16 @@ describe('Authorization Middleware', () => {
     const httpResponse = await sut.handle({})
     expect(httpResponse).toHaveProperty('statusCode', StatusCode.FORBIDDEN)
     expect(httpResponse).toHaveProperty('body', new AccessDeniedError())
+  })
+
+  it('Should to able to call confirmAccessTokenUseCase', async () => {
+    const { sut, confirmAccessTokenUseCase } = makeSUT()
+    const confirmSpy = jest.spyOn(confirmAccessTokenUseCase, 'confirm')
+    const httpRequest = makeFakeHttpRequest()
+
+    await sut.handle(httpRequest)
+    expect(confirmSpy).toHaveBeenCalledWith(
+      httpRequest.headers.authorization
+    )
   })
 })
