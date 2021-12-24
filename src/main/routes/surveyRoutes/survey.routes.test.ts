54873c6fca9f2e1b5723f45eb422d1706437465e
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-24 14:46:28 -0300

    test: ensure return 403 if no has access token provided

--- src/main/routes/surveyRoutes/survey.routes.test.ts
+++ src/main/routes/surveyRoutes/survey.routes.test.ts
@@ -3,6 +3,8 @@ import request from 'supertest'
 import { app } from '../../app'
 import { MongoHelperConnection } from '../../../infra/repositories/database/mongodb/helpers/MongoHelperConnection'
 import { AddSurveyRequestDTO } from '../../../domain/useCases/AddSurveyUseCase'
+import { AddAccountRequestDTO } from '../../../domain/useCases/AddAccountUseCase'
+import { Role } from '../../../domain/protocols/Role'
 
 const { MONGO_URL } = process.env
 
@@ -13,8 +15,31 @@ const makeFakeSurvey = (): AddSurveyRequestDTO => ({
   ]
 })
 
+const makeAdminAccount = (): AddAccountRequestDTO => ({
+  name: 'adm',
+  email: 'adm@mail.com',
+  password: '$2a$12$9yCYZDQmj.9skVhaMbr/5eAA3g4wI5/zIaXm1/z27MnQLHRTWPDTm',
+  role: Role.ADMIN
+})
+
 describe('survey routes', () => {
-  beforeAll(async () => await MongoHelperConnection.connect(MONGO_URL))
+  let adminAccountToken: string
+
+  beforeAll(async () => {
+    await MongoHelperConnection.connect(MONGO_URL)
+    const accountsCollection = await MongoHelperConnection
+      .getCollection('accounts')
+
+    await accountsCollection.insertOne(makeAdminAccount())
+    const response = await request(app)
+      .post('/api/v1/login/sign-in')
+      .send({
+        email: 'adm@mail.com',
+        password: 'teste'
+      })
+
+    adminAccountToken = response.body.accessToken
+  })
   afterAll(async () => await MongoHelperConnection.disconnect())
   afterEach(async () => {
     const collection = await MongoHelperConnection
@@ -23,11 +48,11 @@ describe('survey routes', () => {
   })
 
   describe('Add Survey', () => {
-    it('should return 201 on success', async () => {
+    it('should return 403 if no access token is provided', async () => {
       await request(app)
         .post('/api/v1/survey')
         .send(makeFakeSurvey())
-        .expect(201)
+        .expect(403)
     })
   })
 })
