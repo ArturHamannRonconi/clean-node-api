44ba69230fe30d263f9270d65eba6b00901b8d93
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-27 14:51:01 -0300

    feat: ensure SurveyMongoRepository find all surveys

--- src/infra/repositories/database/mongodb/SurveyRepository/SurveyMongoRepository.ts
+++ src/infra/repositories/database/mongodb/SurveyRepository/SurveyMongoRepository.ts
@@ -1,8 +1,21 @@
 import { Survey } from '../../../../../domain/models'
 import { MongoHelperConnection } from '../helpers/MongoHelperConnection'
 import { AddSurveyRepository, AddSurveyRepositoryRequestDTO } from '../../../../../data/protocols/repositories/SurveyRepository'
+import { FindSurveyRepository } from '../../../../../data/protocols/repositories/SurveyRepository/FindSurveyRepository'
 
-class SurveyMongoRepository implements AddSurveyRepository {
+class SurveyMongoRepository implements AddSurveyRepository, FindSurveyRepository {
+  async all (): Promise<Survey[]> {
+    const surveysCollection = await MongoHelperConnection
+      .getCollection('surveys')
+
+    const surveys = await surveysCollection
+      .find()
+      .map(survey => MongoHelperConnection.map<Survey>(survey))
+      .toArray()
+
+    return surveys
+  }
+
   async add (survey: AddSurveyRepositoryRequestDTO): Promise<Survey> {
     const surveysCollection = await MongoHelperConnection
       .getCollection('surveys')
