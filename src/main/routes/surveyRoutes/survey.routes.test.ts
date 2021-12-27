a2cb0284372523b2dbcd9efca07f714fed025ab2
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-27 16:03:52 -0300

    test: ensure route is functional

--- src/main/routes/surveyRoutes/survey.routes.test.ts
+++ src/main/routes/surveyRoutes/survey.routes.test.ts
@@ -24,32 +24,32 @@ const makeAdminAccount = (): AddAccountRequestDTO => ({
 describe('survey routes', () => {
   let adminAccountToken: string
 
-  beforeAll(async () => {
-    await MongoHelperConnection.connect(MONGO_URL)
-    const accountsCollection = await MongoHelperConnection
-      .getCollection('accounts')
-
-    await accountsCollection.insertOne(makeAdminAccount())
-    const response = await request(app)
-      .post('/api/v1/login/sign-in')
-      .send({
-        email: 'adm@mail.com',
-        password: 'teste'
-      })
-
-    adminAccountToken = response.body.accessToken
-  })
-  afterAll(async () => await MongoHelperConnection.disconnect())
-  afterEach(async () => {
-    const surveysCollection = await MongoHelperConnection
-      .getCollection('surveys')
-    const accountsCollection = await MongoHelperConnection
-      .getCollection('accounts')
-    await surveysCollection.deleteMany({})
-    await accountsCollection.deleteMany({})
-  })
-
   describe('Add Survey', () => {
+    beforeAll(async () => {
+      await MongoHelperConnection.connect(MONGO_URL)
+      const accountsCollection = await MongoHelperConnection
+        .getCollection('accounts')
+
+      await accountsCollection.insertOne(makeAdminAccount())
+      const response = await request(app)
+        .post('/api/v1/login/sign-in')
+        .send({
+          email: 'adm@mail.com',
+          password: 'teste'
+        })
+
+      adminAccountToken = response.body.accessToken
+    })
+    afterAll(async () => await MongoHelperConnection.disconnect())
+    afterEach(async () => {
+      const surveysCollection = await MongoHelperConnection
+        .getCollection('surveys')
+      const accountsCollection = await MongoHelperConnection
+        .getCollection('accounts')
+      await surveysCollection.deleteMany({})
+      await accountsCollection.deleteMany({})
+    })
+
     it('should return 201 on success', async () => {
       await request(app)
         .post('/api/v1/survey')
@@ -62,6 +62,66 @@ describe('survey routes', () => {
       await request(app)
         .post('/api/v1/survey')
         .send(makeFakeSurvey())
+        .expect(403)
+    })
+  })
+
+  describe('Load all surveys', () => {
+    beforeAll(async () => {
+      await MongoHelperConnection.connect(MONGO_URL)
+      const accountsCollection = await MongoHelperConnection
+        .getCollection('accounts')
+
+      await accountsCollection.insertOne(makeAdminAccount())
+      const response = await request(app)
+        .post('/api/v1/login/sign-in')
+        .send({
+          email: 'adm@mail.com',
+          password: 'teste'
+        })
+
+      adminAccountToken = response.body.accessToken
+    })
+    afterAll(async () => await MongoHelperConnection.disconnect())
+    afterEach(async () => {
+      const surveysCollection = await MongoHelperConnection
+        .getCollection('surveys')
+      const accountsCollection = await MongoHelperConnection
+        .getCollection('accounts')
+      await surveysCollection.deleteMany({})
+      await accountsCollection.deleteMany({})
+    })
+
+    it('Should return 200 on success', async () => {
+      await Promise.all([
+        request(app)
+          .post('/api/v1/survey')
+          .set(
+            'authorization',
+            `Bearer ${adminAccountToken}`
+          )
+          .send(makeFakeSurvey()),
+        request(app)
+          .post('/api/v1/survey')
+          .set(
+            'authorization',
+            `Bearer ${adminAccountToken}`
+          )
+          .send(makeFakeSurvey())
+      ])
+
+      const response = await request(app)
+        .get('/api/v1/survey')
+        .set('authorization', `Bearer ${adminAccountToken}`)
+        .expect(200)
+
+      expect(response.body.surveys).toHaveLength(2)
+      expect(response.body).toHaveProperty('surveys')
+    })
+
+    it('Should return 403 if no access token is provided', async () => {
+      await request(app)
+        .get('/api/v1/survey')
         .expect(403)
     })
   })
