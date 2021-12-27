b2a061142211f9118cecb6bc9ad0c684051861db
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-27 16:03:36 -0300

    feat: ensure route is functional

--- src/main/factories/controllers/LoadSurveysControllerFactory.ts
+++ src/main/factories/controllers/LoadSurveysControllerFactory.ts
@@ -1,11 +1,14 @@
 import { Controller } from '../../../presentation/protocols'
 import { loadSurveysUseCaseFactory } from '../useCases/LoadSurveysUseCaseFactory'
 import { LoadSurveysController } from '../../../presentation/controllers/survey/LoadSurveysController'
+import { loggerDecoratorFactory } from '../decorators/LoggerDecoratorFactory'
 
 const loadSurveysControllerFactory = (): Controller<void> => {
-  return new LoadSurveysController(
+  const loadSurveysController = new LoadSurveysController(
     loadSurveysUseCaseFactory()
   )
+
+  return loggerDecoratorFactory<void>(loadSurveysController)
 }
 
 export { loadSurveysControllerFactory }
--- src/main/routes/surveyRoutes/survey.routes.ts
+++ src/main/routes/surveyRoutes/survey.routes.ts
@@ -2,12 +2,18 @@ import { Router } from 'express'
 import { AuthorizationMiddlewareExpressAdapter } from '../../../infra/middlewares/express/AuthorizationMiddlewareExpressAdapter'
 
 import { AddSurveyControllerExpressAdapter } from '../../../infra/router/express/AddSurveyControllerExpressAdapter'
+import { LoadSurveysConrollerExpressAdapter } from '../../../infra/router/express/LoadSurveysControllerExpressAdapter'
 
 const surveyRoutes = Router()
 
-surveyRoutes.post('/',
-  AuthorizationMiddlewareExpressAdapter.handle,
-  AddSurveyControllerExpressAdapter.handle
-)
+surveyRoutes.route('/')
+  .post(
+    AuthorizationMiddlewareExpressAdapter.handle,
+    AddSurveyControllerExpressAdapter.handle
+  )
+  .get(
+    AuthorizationMiddlewareExpressAdapter.handle,
+    LoadSurveysConrollerExpressAdapter.handle
+  )
 
 export { surveyRoutes }
