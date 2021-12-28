6b469c1a449e2223ee1999aa9f6d4c1ace2fbd3f
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-28 10:50:28 -0300

    feat: ensure SurveyMongoRepository find survey by id

--- src/infra/repositories/database/mongodb/SurveyRepository/SurveyMongoRepository.ts
+++ src/infra/repositories/database/mongodb/SurveyRepository/SurveyMongoRepository.ts
@@ -2,8 +2,20 @@ import { Survey } from '../../../../../domain/models'
 import { MongoHelperConnection } from '../helpers/MongoHelperConnection'
 import { AddSurveyRepository, AddSurveyRepositoryRequestDTO } from '../../../../../data/protocols/repositories/SurveyRepository'
 import { FindSurveyRepository } from '../../../../../data/protocols/repositories/SurveyRepository/FindSurveyRepository'
+import { Guid } from '../../../../../domain/protocols/Guid'
+import { ObjectId } from 'mongodb'
 
 class SurveyMongoRepository implements AddSurveyRepository, FindSurveyRepository {
+  async byId (surveyId: Guid): Promise<Survey> {
+    const surveysCollection = await MongoHelperConnection
+      .getCollection('surveys')
+
+    const survey = await surveysCollection
+      .findOne(new ObjectId(surveyId))
+
+    return MongoHelperConnection.map<Survey>(survey)
+  }
+
   async all (): Promise<Survey[]> {
     const surveysCollection = await MongoHelperConnection
       .getCollection('surveys')
