ccac15c88122cf036d96f35beb45c7adee3e9384
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-28 12:45:10 -0300

    feat: ensure SurveyResultMongoRepository insert surveyResult if not exists

---
+++ src/infra/repositories/database/mongodb/SurveyResultRepository/SurveyResultMongoRepository.ts
@@ -0,0 +1,21 @@
+import { SaveSurveyResultRepository } from '../../../../../data/protocols/repositories/SurveyResultRepository/SaveSurveyResultRepository'
+import { SaveSurveuResultRepositoryRequestDTO } from '../../../../../data/protocols/repositories/SurveyResultRepository/SaveSurveyResultRepositoryRequestDTO'
+import { Guid } from '../../../../../domain/protocols/Guid'
+import { MongoHelperConnection } from '../helpers/MongoHelperConnection'
+
+class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save ({ accountId, answer, surveyId }: SaveSurveuResultRepositoryRequestDTO): Promise<Guid> {
+    const surveysResultsCollection = await MongoHelperConnection
+      .getCollection('surveysResults')
+
+    const { value: survey } = await surveysResultsCollection.findOneAndUpdate(
+      { surveyId, accountId },
+      { $set: { answer } },
+      { upsert: true, returnDocument: 'after' }
+    )
+
+    return survey._id
+  }
+}
+
+export { SurveyResultMongoRepository }
