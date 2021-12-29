93f7c9f91928af301ff237a257c3de857b80338c
ArturHamannRonconi <arturhronconi@gmail.com>
2021-12-28 21:02:19 -0300

    test: ensure returns 403 if answer not exists

--- src/presentation/controllers/surveyResult/AddSurveyResultController/AddSurveyResultController.spec.ts
+++ src/presentation/controllers/surveyResult/AddSurveyResultController/AddSurveyResultController.spec.ts
@@ -3,7 +3,7 @@ import { Survey } from '../../../../domain/models'
 import { LoadSurveyByIdRequestDTO, LoadSurveyByIdResponseDTO, LoadSurveyByIdUseCase } from '../../../../domain/useCases/LoadSurveyByIdUseCase'
 import { HttpRequest, StatusCode } from '../../../protocols/http'
 import { AddSurveyResultHttpRequestBody } from './AddSurveyResultHttpRequestBody'
-import { AccessDeniedError } from '../../../utils/errors'
+import { InvalidParamError, ServerError } from '../../../utils/errors'
 
 const makeFakeSurvey = (): Survey => ({
   id: 'any_survey_id',
@@ -15,7 +15,11 @@ const makeFakeSurvey = (): Survey => ({
 })
 
 const makeFakeHttpRequest = (): HttpRequest<AddSurveyResultHttpRequestBody> => ({
-  params: { survey_id: 'any_survey_id' }
+  params: { survey_id: 'any_survey_id' },
+  body: {
+    accountId: 'any_account_id',
+    answer: 'any_answer'
+  }
 })
 
 const makeloadSurveyByIdUseCase = (): LoadSurveyByIdUseCase => {
@@ -58,7 +62,7 @@ describe('Add Survey Result Controller', () => {
     })
   })
 
-  it('Should return 403 if LoadSurveyByIdUseCase return null', async () => {
+  it('Should return 403 if surveyId not exists', async () => {
     const { sut, loadSurveyByIdUseCase } = makeSUT()
     jest
       .spyOn(loadSurveyByIdUseCase, 'load')
@@ -68,6 +72,35 @@ describe('Add Survey Result Controller', () => {
 
     const httpResponse = await sut.handle(makeFakeHttpRequest())
     expect(httpResponse).toHaveProperty('statusCode', StatusCode.FORBIDDEN)
-    expect(httpResponse.body).toHaveProperty('error', new AccessDeniedError().message)
+    expect(httpResponse.body).toHaveProperty(
+      'error', new InvalidParamError('survey_id').message
+    )
+  })
+
+  it('Should return 403 if answer not exists', async () => {
+    const { sut } = makeSUT()
+
+    const httpresponse = await sut.handle({
+      ...makeFakeHttpRequest(),
+      body: {
+        accountId: 'any_account_id',
+        answer: 'answer_not_exists'
+      }
+    })
+
+    expect(httpresponse).toHaveProperty('statusCode', StatusCode.FORBIDDEN)
+    expect(httpresponse.body).toHaveProperty('error', new InvalidParamError('answer').message)
+  })
+
+  it('Should return 500 if LoadSurveyByIdUseCase throws', async () => {
+    const { sut, loadSurveyByIdUseCase } = makeSUT()
+
+    jest
+      .spyOn(loadSurveyByIdUseCase, 'load')
+      .mockImplementationOnce(async () => { throw new Error() })
+
+    const httpResponse = await sut.handle(makeFakeHttpRequest())
+    expect(httpResponse).toHaveProperty('statusCode', StatusCode.INTERNAL_SERVER)
+    expect(httpResponse).toHaveProperty('body', new ServerError())
   })
 })
